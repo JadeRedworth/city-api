18c6a6cd8a36216703983dc638f22e6efa8ef621
Rik Gibson <rik.gibson@oracle.com>
2017-12-12 15:27:41 +0000

    use jdk-8-alpine

--- sql/citydb.sql
+++ sql/citydb.sql
@@ -1,3 +1,10 @@
+create database if not exists citydb;
+
+use citydb;
+
+CREATE USER IF NOT EXISTS 'test'@'%' IDENTIFIED BY 'welcome1';
+GRANT ALL ON citydb.* TO 'test'@'%';
+
 CREATE TABLE city (
   name VARCHAR(255) NOT NULL PRIMARY KEY
 ) CHARACTER SET utf8
@@ -6,3 +13,4 @@ CREATE TABLE city (
 INSERT INTO city (name) VALUES ("Amsterdam");
 INSERT INTO city (name) VALUES ("Bristol");
 INSERT INTO city (name) VALUES ("London");
+INSERT INTO city (name) VALUES ("San Francisco");
