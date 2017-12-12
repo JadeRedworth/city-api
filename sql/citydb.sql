924c68db663694fe8815dc87d0bab2c731c81336
Rik Gibson <rik.gibson@oracle.com>
2017-12-12 11:37:12 +0000

    Get cities from local db

---
+++ sql/citydb.sql
@@ -0,0 +1,8 @@
+CREATE TABLE city (
+  name VARCHAR(255) NOT NULL PRIMARY KEY
+) CHARACTER SET utf8
+  COLLATE utf8_general_ci;
+
+INSERT INTO city (name) VALUES ("Amsterdam");
+INSERT INTO city (name) VALUES ("Bristol");
+INSERT INTO city (name) VALUES ("London");
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,5 +1,12 @@
 package demo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +21,37 @@ public class Hello {
 		return "Hello World!";
 	}
 
+	@RequestMapping("/api/v1/city")
+	public String getCities() {
+		String[] cities = this.getCitiesFromDB();
+		return Arrays.deepToString(cities);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Hello.class, args);
+	}
+
+	private String[] getCitiesFromDB() {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		ArrayList<String> allCities = new ArrayList<String>();
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/citydb?user=test&password=welcome1&useSSL=false");
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("select name from city");
+
+			while (rs.next()) {
+				String cityName = rs.getString("name");
+				System.out.println("Found city: " + cityName);
+				allCities.add(cityName);
+			}			
+		}
+		catch (SQLException ex) {
+			System.out.println("Error getting JDBC connection:" + ex);
+		}
+
+		return allCities.toArray(new String[0]);
 	}
 }
